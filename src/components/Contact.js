6e9e852d9acb10785ac4327d7c946390b86c7f52
arturexpone <arturexpone@yandex.ru>
2020-06-11 14:37:33 +0300

    added icon library and event for i

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 
 export const Contact = ({name, email, phone}) => {
 
+    const onShowClick = () => {
+
+    }
+
     return (
         <div className='card card-body mb-3'>
-            <h4>{name}</h4>
+            <h4>{`${name} `}
+                <i onClick={onShowClick} className='fas fa-sort-down' />
+            </h4>
             <ul className='list-group'>
                 <li className='list-group-item'>Email: {email}</li>
                 <li className='list-group-item'>Phone: {phone}</li>
