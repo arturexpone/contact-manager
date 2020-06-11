0ad568c88de07282cb9565f7f06ee14531ef73ab
arturexpone <arturexpone@yandex.ru>
2020-06-11 19:25:48 +0300

    add 404

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import {Header} from './components/layout/Header';
+import {About} from './components/pages/About';
+import {NotFound} from './components/pages/NotFound';
 import {ContactsProvider} from './components/contacts/Contacts';
-import {About} from "./components/pages/About";
-import {AddContactForm} from "./components/contacts/AddContact";
+import {AddContactForm} from './components/contacts/AddContact';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 
 
@@ -24,6 +26,7 @@ function App() {
                     <Route exact path='/' component={ContactsProvider} />
                     <Route exact path='/contact/add' component={AddContactForm} />
                     <Route exact path='/about' component={About} />
+                    <Route component={NotFound} />
                 </Switch>
             </div>
 
