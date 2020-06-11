a83fd0824e73b4743bfa1e9a28b11e8c776ff6b5
arturexpone <arturexpone@yandex.ru>
2020-06-11 18:57:42 +0300

    added pages and configured routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import {ContactsProvider} from './components/contacts/Contacts';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 import {Header} from './components/layout/Header';
+import {ContactsProvider} from './components/contacts/Contacts';
+import {About} from "./components/pages/About";
+import {AddContactForm} from "./components/contacts/AddContact";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {AddContactForm} from "./components/contacts/AddContact";
+
+
 
 
 
 function App() {
   return (
-    <div className="App">
+      <Router>
+        <div className="App">
 
-        <Header branding={'Contact manager'}/>
-        <div className="container">
-            <AddContactForm />
-            <ContactsProvider />
+            <Header branding={'Contact manager'}/>
+            <div className="container">
+                <Switch>
+                    <Route exact path='/' component={ContactsProvider} />
+                    <Route exact path='/contact/add' component={AddContactForm} />
+                    <Route exact path='/about' component={About} />
+                </Switch>
+            </div>
+
         </div>
-
-    </div>
+      </Router>
   );
 }
 
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -11,6 +11,10 @@ const Contacts = (props) => {
 
     return (
         <>
+            <h1 className="display-4 mb-2">
+                <span className="text-danger">Contact </span>
+                List
+            </h1>
             {users}
         </>
     )
