47d746ec4a9c69d60aab3c6a78e2683bfaf3fa43
arturexpone <arturexpone@yandex.ru>
2020-06-11 13:07:09 +0300

    create contact component

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Contact = () => {
+    return (
+        <div>
+            <h4>John Doe</h4>
+            <ul>
+                <li>Email: jdoe@gmail.com</li>
+                <li>Phone: 555-555-5555</li>
+            </ul>
+        </div>
+    )
+}
